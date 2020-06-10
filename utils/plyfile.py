19e8c036f3f763f4006dc71f6f876e818fb88f3e
Vasyl Haievyi <vasyl.haievyi@student.uj.edu.pl>
2020-06-10 21:41:04 +0200

    truncated files to have the same size 100k points

--- utils/plyfile.py
+++ utils/plyfile.py
@@ -949,4 +949,4 @@ def load_photogrammetry(filename: str, split: str = ' ') -> _np.ndarray:
                 [float(num) for num in line.split(split)][:-1]
             ))
 
-    return _np.array(result)
+    return _np.array(result[:100_000])
